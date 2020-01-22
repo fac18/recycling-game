bbb74e6b291fb4748599b74ecbe9ccea5237ed25
Reda <redahaq@gmail.com>
2020-01-22 10:55:44 +0000

    show svgs on page, test dnd functionality

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { DragDropContainer, DropTarget } from 'react-drag-drop-container';
+import {
+  ReactComponent as CoffeeCup
+  } from '../../assets/coffee-cup.svg';
+
+import {
+  ReactComponent as RecyclingBin
+  } from '../../assets/recycle-bin.svg';
+
 
 const GameScreen = () => {
   return (
+    // <div>
+    //   <h2>This is the Game in action</h2>
+    // </div>
     <div>
-      <h2>This is the Game in action</h2>
+      <DragDropContainer targetKey="middle" >
+          <div> < CoffeeCup /> </div>
+      </DragDropContainer>
+
+      <DropTarget targetKey="middle" >
+          <div> < RecyclingBin /> </div>
+      </DropTarget>
     </div>
+
   );
 };
 
