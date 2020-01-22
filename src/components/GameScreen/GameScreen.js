5982c0241da58494eba2f866cd6a7895cb6c9e72
Jamie Carter <jc2820@hotmail.com>
2020-01-22 14:56:39 +0000

    adds basic drag and drop

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,56 +1,25 @@
 import React from "react";
-import { DragDropContainer, DropTarget } from 'react-drag-drop-container';
-// import Item from './Item';
-// import Bin from './Bin';
-import {
-  ReactComponent as CoffeeCup
-  } from '../../assets/coffee-cup.svg';
+import { DragDropContainer, DropTarget } from "react-drag-drop-container";
 
-import {
-  ReactComponent as RecyclingBin
-  } from '../../assets/recycle-bin.svg';
+import { ReactComponent as RecyclingBin } from "../../assets/recycle-bin.svg";
 
+import { ReactComponent as GlassBottle } from "../../assets/glass-bottle.svg";
 
 const GameScreen = () => {
+  const dropReaction = e => {
+    e.containerElem.style.visibility = "hidden";
+  };
+
   return (
     <div>
-      {/* <div>
-        <Item targetKey="middle">
-          <CoffeeCup/>
-        </Item>
-      </div>
-
-    <div>
-      < Bin targetKey="middle">
-         < RecyclingBin />
-      </Bin>
-    </div> */}
-
-      <DragDropContainer targetKey="middle" >
-        
-          <div> <h1>HELLO JAMIE</h1> </div>
-         
+      <DragDropContainer targetKey="middle">
+        <GlassBottle />
       </DragDropContainer>
 
-      <DropTarget targetKey="middle"> 
-          <div> <RecyclingBin/> </div>
+      <DropTarget targetKey="middle" onHit={dropReaction}>
+        <RecyclingBin />
       </DropTarget>
-
-
-
     </div>
-
-//     <DragDropContainer 
-//     targetKey="middle" 
-//     // dragData={some object} 
-//     onDrop={some method}
-//     onDragStart={some method} 
-//     onDrag={some method} 
-//     onDragEnd={some method} 
-// >
-//     <div>Drag Me!</div>
-// </DragDropContainer>
-
   );
 };
 
