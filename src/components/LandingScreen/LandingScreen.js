d164e13ddcc68f921a9d971d22d4e5a71e109c61
Roshan Thaabith <roshlarosh@gmail.com>
2020-01-27 16:16:54 +0000

    add route to game screen from the landing page

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -1,24 +1,35 @@
 import React from "react";
 // import styled from "styled-components";
+import Button from "../Button";
+import { useHistory } from "react-router-dom";
 
 import { ReactComponent as NightBackground } from "../../assets/night-background.svg";
 import { ReactComponent as Earth } from "../../assets/earth.svg";
-// import { ReactComponent as Title } from "../../assets/reduce, reuse, recycle.svg";
-import { ReactComponent as PlayButton } from "../../assets/play-button-purple.svg";
-import { ReactComponent as HowToPLay } from "../../assets/button-secondary.svg";
-import { ReactComponent as FunFacts } from "../../assets/button-secondary.svg";
 import { ReactComponent as Stars } from "../../assets/stars.svg";
 
 const LandingScreen = () => {
+  const history = useHistory();
+  const startGame = () => {
+    history.push("/");
+  };
+
+  // const goToFunFacts = () => {
+  //   history.push("/fun-facts");
+  // };
+
+  // const goToHowToPlay = () => {
+  //   history.push("/how-to-play");
+  // };
+
   return (
     <div>
       <h1>reduce, reuse, recycle</h1>
       <NightBackground />
       <Stars />
-      <PlayButton />
       <Earth />
-      <HowToPLay />
-      <FunFacts />
+      <Button handleClick={startGame} label="Let's Play" />
+      {/* <Button handleClick={goToFunFacts} label="Let's Play" />
+      <Button handleClick={goToHowToPlay} label="Let's Play" /> */}
     </div>
   );
 };
