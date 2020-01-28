7b15d44037e518949e7493db0e67b4334cfd0a78
Ayub Mohamud <a.mohamud1@hotmail.com>
2020-01-28 15:27:59 +0000

    attempt to fix how to play box

--- src/components/LandingScreen/HowToPlayModal.js
+++ src/components/LandingScreen/HowToPlayModal.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { ReactComponent as RecycleBin } from "../../assets/recycle-bin.svg";
+import { ReactComponent as BlackBin } from "../../assets/waste-bin-tidyman.svg";
+import { ReactComponent as CompostBin } from "../../assets/compostable-bin.svg";
+
 
 const HowToPlayBox = styled.div`
-  background-color: rgba(23, 235, 16, 0.479);
-  padding: 3em;
+background: linear-gradient(180deg, #21B2D3 0%, #7ABEFD 100%), linear-gradient(180deg, #68C2CD 0%, #7ABEFD 100%);  padding: 3em;
   position: fixed;
   width: 80%;
   height: 50%;
@@ -13,7 +16,13 @@ const HowToPlayBox = styled.div`
   overflow: scroll;
 `;
 
+
 const Box = styled.div`
+  margin: 50px;
+  display: flex;
+  felx-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 50vw;
   height: 20vh;
   border-radius: 10px;
@@ -22,17 +31,24 @@ const Box = styled.div`
   padding: 20px;
 `;
 
+const howToBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+
 const HowToPlayModal = ({ handleClose }) => {
   return (
     <HowToPlayBox>
       <section className="modal-main">
         <h2>How to play?</h2>
         <h2>This is how to play the game</h2>
-        <Box>Drag and drop all items that CAN BE RECYCLED into this bin.</Box>
-        <Box>Drag and drop all the items that CANNOT BE RECYCLED into this bin</Box>
-        <Box>Drag and drop all items that are FOOD WASTE into this bin</Box>
-        <Box>This is your LIFE. If you put the item in the worng bin you lose a life</Box>
-        <Box>If you put the item in the right bin you get POINTS</Box>
+        <div className="howToBox"><RecycleBin width="30%"/><Box> Drag and drop all items that CAN BE RECYCLED into this bin.</Box></div>
+        <div className="howToBox"><BlackBin width="30%"/><Box>Drag and drop all the items that CANNOT BE RECYCLED into this bin</Box></div>
+        <div className="howToBox"><CompostBin width="30%"/><Box>Drag and drop all items that are FOOD WASTE into this bin</Box></div>
+        <div className="howToBox"><Box>This is your LIFE. If you put the item in the worng bin you lose a life</Box></div>
+        <div className="howToBox"><Box>If you put the item in the right bin you get POINTS</Box></div>
         {/* {children} */}
         <button onClick={handleClose}>close</button>
       </section>
