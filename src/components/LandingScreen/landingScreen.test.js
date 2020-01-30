6a72b647180ac30dba405ffd64206a14ec4fc30b
Ayub Mohamud <a.mohamud1@hotmail.com>
2020-01-30 10:50:17 +0000

    attempt to wrtie landingscreen test

--- src/components/LandingScreen/landingScreen.test.js
+++ src/components/LandingScreen/landingScreen.test.js
@@ -1,6 +1,11 @@
-test('1 + 1 is 2', () => {
-    const actual = 1 + 1;
-    const expected = 2;
-    expect(actual).toBe(expected);
-  });
+import React from "react";
+import { render, getByText } from '@testing-library/react';
+import LandingScreen from './LandingScreen';
+
+
+test("play again button is on the page", ()=>{
+  const {getByText,debug} = render(<LandingScreen/>)
+  const button = getByText("Lets Play Again!")
+  expect(button).toBeInDocument();
+})
   
