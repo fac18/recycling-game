f30f6fb6fcd18bb1d55bc8ffc9e726760c8649e3
Roshan Thaabith <roshlarosh@gmail.com>
2020-01-30 11:58:05 +0000

    some tests

--- src/components/LandingScreen/landingScreen.test.js
+++ src/components/LandingScreen/landingScreen.test.js
@@ -1,6 +1,5 @@
-test('1 + 1 is 2', () => {
-    const actual = 1 + 1;
-    const expected = 2;
-    expect(actual).toBe(expected);
-  });
-  
+test("1 + 1 is 2", () => {
+  const actual = 1 + 1;
+  const expected = 2;
+  expect(actual).toBe(expected);
+});
