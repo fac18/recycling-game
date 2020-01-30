c55ad59e2b9288a72701c4b9feeae1c3c0f7898f
Reda <redahaq@gmail.com>
2020-01-30 15:03:59 +0000

    add close cross to landing screen modals as position sticky was not working well

--- src/components/LandingScreen/HowToPlayModal.js
+++ src/components/LandingScreen/HowToPlayModal.js
@@ -13,46 +13,69 @@ import {
   MessageBox,
   BoxMessage,
   BadgeBox,
-  OrangeText
+  OrangeText,
+  CloseCross
 } from "../MasterCss";
 
 
 const HowToPlayBox = styled.div`
-background-image: linear-gradient(#21b2d3, #7abefd);
-padding: 2em;
+// background-image: linear-gradient(#21b2d3, #7abefd);
+// padding: 2em;
+//   position: fixed;
+//   width: 90vw;
+//   height: 85vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+//   overflow: scroll;
+//   z-index: 3000;
+//   border-radius: 25px;
+//   border: 2px solid #08345C;
+//   filter: drop-shadow(4px 4px 4px #08345C);
+
+
+  background-image: linear-gradient(#21b2d3, #7abefd);
+  padding: 2em;
   position: fixed;
   width: 90vw;
   height: 85vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   overflow: scroll;
+  display: block;
   z-index: 3000;
   border-radius: 25px;
   border: 2px solid #08345C;
   filter: drop-shadow(4px 4px 4px #08345C);
+  text-align: center;
+`;
+
+
+const CenterChildren = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const HowToPlayModal = ({ handleClose }) => {
   return (
     <HowToPlayBox>
-      {/* <section className="modal-main"> */}
-
+        <CloseCross onClick={handleClose} />
         <PageHeader>How to play</PageHeader>
-        {/* <TextH2>This is how to play the game</TextH2> */}
-        <MessageBox><BadgeBox><RecycleBin width="20%" height="70%"/><BoxMessage>Drag and drop items that CAN BE RECYCLED into this bin.</BoxMessage></BadgeBox></MessageBox>
-        <MessageBox><BadgeBox><BlackBin width="20%" height="70%"/><BoxMessage>Drag and drop items that CANNOT BE RECYCLED into this bin</BoxMessage></BadgeBox></MessageBox>
-        <MessageBox><BadgeBox><CompostBin width="20%" height="70%"/><BoxMessage>Drag and drop items that are FOOD WASTE into this bin</BoxMessage></BadgeBox></MessageBox>
-        <MessageBox><BadgeBox><Heart width="20%" /><BoxMessage>These are your LIVES. If you put the item in the wrong bin you lose a life</BoxMessage></BadgeBox></MessageBox>
-        <MessageBox><BadgeBox><StarFish width="20%" /><BoxMessage>If you put an item in the right bin you get starfish POINT</BoxMessage></BadgeBox></MessageBox>
-      <Cross onClick={handleClose} />
+        <CenterChildren>
+        <MessageBox ><BadgeBox><RecycleBin width="20%" height="70%"/><BoxMessage>Drag and drop items that CAN BE RECYCLED into this bin.</BoxMessage></BadgeBox></MessageBox>
+        <MessageBox ><BadgeBox><BlackBin width="20%" height="70%"/><BoxMessage>Drag and drop items that CANNOT BE RECYCLED into this bin</BoxMessage></BadgeBox></MessageBox>
+        <MessageBox ><BadgeBox><CompostBin width="20%" height="70%"/><BoxMessage>Drag and drop items that are FOOD WASTE into this bin</BoxMessage></BadgeBox></MessageBox>
+        <MessageBox ><BadgeBox><Heart width="20%" /><BoxMessage>These are your LIVES. If you put the item in the wrong bin you lose a life</BoxMessage></BadgeBox></MessageBox>
+        <MessageBox ><BadgeBox><StarFish width="20%" /><BoxMessage>If you put an item in the right bin you get starfish POINT</BoxMessage></BadgeBox></MessageBox>
+        </CenterChildren>
 
-        {/* {children} */}
         <Button primary handleClick={handleClose} label="Close"></Button>
-      {/* </section> */}
+
     </HowToPlayBox>
   );
 };
